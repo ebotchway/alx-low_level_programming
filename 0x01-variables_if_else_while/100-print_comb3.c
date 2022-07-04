a61abca5e5688a3e4b956b8eb4213011a21c97ba
Emmanuel Kwaku Botchway <emmlquake@live.com>
2022-07-04 16:41:08 +0000

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,8 +4,8 @@
 /**
  * main - Prints all possible combinations of two different digits,
  * in ascending order, separated by a comma followed by a space.
- *     
- * Return: Always 0.     
+ *
+ * Return: Always 0
  */
 
 
