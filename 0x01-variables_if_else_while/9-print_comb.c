ff87484db18c21f47044699112cbc4c0e4d27ff0
Emmanuel Kwaku Botchway <emmlquake@live.com>
2022-07-04 17:39:55 +0000

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 
-
 /**
- * main - Prints all possible combinations of single-digit numbers
+ * main - Prints all possible combinations of single-digit numbers.
  *
- * Return: Always 0
+ * Return: Always 0.
  */
-
 int main(void)
-
 {
 	int num;
 
@@ -17,9 +14,12 @@ int main(void)
 		putchar((num % 10) + '0');
 		if (num == 9)
 			continue;
+
 		putchar(',');
 		putchar(' ');
 	}
+
 	putchar('\n');
+
 	return (0);
 }
