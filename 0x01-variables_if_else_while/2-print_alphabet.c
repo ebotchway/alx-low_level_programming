a829713266ab7685b981d2ef467dc8dee2cc41e3
Emmanuel Kwaku Botchway <emmlquake@live.com>
2022-07-04 16:43:30 +0000

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,33 +3,18 @@
 
 
 /**
+ * main - Prints the alphabet in lowercase.
  *
- *  * main - Prints the alphabet in lowercase.
- *
- *   *
- *
- *    * Return: Always 0.
- *
- *     */
+ * Return: Always 0
+ */
 
 int main(void)
 
 {
+	char letter;
 
-		char letter;
-
-
-
-			for (letter = 'a'; letter <= 'z'; letter++)
-
-						putchar(letter);
-
-
-
-				putchar('\n');
-
-
-
-					return (0);
-
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	putchar('\n');
+	return (0);
 }
