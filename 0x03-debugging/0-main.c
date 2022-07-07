a12675f4e1140cde868ad43f3e4ba8d6f2a531f3
Emmanuel Kwaku Botchway <emmlquake@live.com>
2022-07-07 11:41:47 -0700

    update

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "main.h"
 
 /**
@@ -15,22 +14,3 @@ int main(void)
 
 	return (0);
 }
-=======
-#include "main.h"
-
-/**
- * main - tests function that prints if integer is positive or negative
- * Return: 0
- */
-
-int main(void)
-{
-	int i;
-
-	i = 0;
-	positive_or_negative(i);
-
-	return (0);
-}
-
->>>>>>> 4c8c9f16e14eb8e56218b52d5d19c0e9bdc8750f
